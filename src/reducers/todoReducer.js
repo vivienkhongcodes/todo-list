54eb7b3f5fd117904f98f68a317f3afa20c9613f
vivienkhongcodes <264569188+vivienkhongcodes@users.noreply.github.com>
2026-09-02 20:28:27 -0400

    Fix reset filter sort direction

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -178,7 +178,7 @@ export function todoReducer(state, action) {
             ...state,
             filterTerm: '',
             sortBy: 'createdAt',
-            sortDirection: 'desc',
+            sortDirection: 'asc',
             filterError: '',
           };  
 
